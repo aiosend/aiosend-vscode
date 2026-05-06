689dade783d30033719ef4ec54b139fb167d8031
NEFORCEO <pirogartem919@gmail.com>
2026-05-06 22:39:13 +0700

    test: create main.py

---
+++ tests/main.py
@@ -0,0 +1,13 @@
+import asyncio
+
+from aiosend import CryptoPay
+
+cp = CryptoPay(token="...")
+
+async def main() -> int:
+    invoice = await cp.create_invoice(amount=5, asset="BTC")
+    return invoice.invoice_id
+
+
+asyncio.run(main())
+    
