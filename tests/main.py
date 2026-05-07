2e7abdc8c95b31ff97c3bd71e426e3f8bd4ce6a8
NEFORCEO <pirogartem919@gmail.com>
2026-05-07 17:38:03 +0700

    test: update main.py

--- tests/main.py
+++ tests/main.py
@@ -5,7 +5,7 @@ from aiosend import CryptoPay
 cp = CryptoPay(token="...")
 
 async def main() -> int:
-    invoice = await cp.create_invoice(amount=5, asset="BTC")
+    invoice =  await cp.create_invoice(amount=5, asset="BTC")
     return invoice.invoice_id
 
 
